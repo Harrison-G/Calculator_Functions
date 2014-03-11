f931ab902c2f68ec925bc88793069b5ad400b3b1
grodinh <adamgrodin@A-Grodins-MacBook-Pro.local>
2014-03-10 22:16:09 -0400

    Added setTrigMode().
    setTrigMode() receives an input of "degrees" or "radians" and changes
    to the designated mode.  If the input is not either one, it will return
    an error.

--- Formulas_and_Identities_from_Trigonometry.py
+++ Formulas_and_Identities_from_Trigonometry.py
@@ -1,1 +1,10 @@
 #Formulas_and_Identities_from_Trigonometry
+trigMode = 1
+
+def setTrigMode(mode):
+    if mode=="degrees":
+        trigMode=math.pi/180
+    elif mode=="radians":
+        trigMode=1
+    else:
+        error
