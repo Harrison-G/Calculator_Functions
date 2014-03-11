400a6cb8aa686f041bf0e08b853f1b96061fb71c
grodinh <grodinh@winchesterthurston.org>
2014-03-10 21:03:01 -0400

    Added Basic Trig Functions

--- Basic_Functions.py
+++ Basic_Functions.py
@@ -1,2 +1,12 @@
 #Defines basic mathematical functions.
 
+import math
+
+def sin(num):
+    return math.sin(num)
+    
+def cos(num):
+    return math.cos(num)
+    
+def tan(num):
+    return math.tan(num)
