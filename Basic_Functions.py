8ad3525d3de700d1880dea34b9776ce91a490c28
grodinh <adamgrodin@A-Grodins-MacBook-Pro.local>
2014-04-05 22:51:36 -0400

    Made Variables Usable In Expressions

--- Basic_Functions.py
+++ Basic_Functions.py
@@ -4,34 +4,34 @@ import math
 import cmath
 import fractions
 
+variables = {}
 
 #Command Line Interface
 
 def commandLine(command):
     if len(command.split('='))==2: #If defining a variable
         if len(command.split('/'))==1: #If variable value is a decimal
-            globals()[command.split('=')[0]]={'mode':'decimal','decimal':float(command.split('=')[1]),'numerator':int(str(fractions.Fraction(str(command.split('=')[1]))).split('/')[0]),'denominator':int(str(fractions.Fraction(str(command.split('=')[1]))).split('/')[1])}
+            if float(command.split('=')[1])%1==0:
+                variables[command.split('=')[0]]={'mode':'decimal','decimal':float(command.split('=')[1]),'numerator':int(str(fractions.Fraction(str(command.split('=')[1]))).split('/')[0]),'denominator':1}
+            else:
+                variables[command.split('=')[0]]={'mode':'decimal','decimal':float(command.split('=')[1]),'numerator':int(str(fractions.Fraction(str(command.split('=')[1]))).split('/')[0]),'denominator':int(str(fractions.Fraction(str(command.split('=')[1]))).split('/')[1])}
+            globals()[command.split('=')[0]]=variables[command.split('=')[0]]['decimal']
         else: #If variable value is a fraction
-            globals()[command.split('=')[0]]={'mode':'fraction','decimal':float(eval(command.split('=')[1])),'numerator':int(str(fractions.Fraction(command.split('=')[1])).split('/')[0]),'denominator':int(str(fractions.Fraction(command.split('=')[1])).split('/')[1])}
-            print(globals()[command.split('=')[0]]['decimal'])
-            print(globals()[command.split('=')[0]]['numerator'])
-            print(globals()[command.split('=')[0]]['denominator'])
+            variables[command.split('=')[0]]={'mode':'fraction','decimal':float(eval(command.split('=')[1])),'numerator':int(str(fractions.Fraction(command.split('=')[1])).split('/')[0]),'denominator':int(str(fractions.Fraction(command.split('=')[1])).split('/')[1])}
+            globals()[command.split('=')[0]]=variables[command.split('=')[0]]['decimal'] #globals()[command.split('=')[0]]=str(variables[command.split('=')[0]]['numerator'])+'/'+str(variables[command.split('=')[0]]['denominator'])
         evalInput="Done."
-    elif str(command) in globals():
-        if globals()[command]['mode']=='fraction':
-            evalInput=str(globals()[command]['numerator'])+"/"+str(globals()[command]['denominator'])
-        elif globals()[command]['mode']=='decimal':
-            evalInput=globals()[command]['decimal']
+    elif str(command) in variables and variables[command]['mode']=='fraction':
+        evalInput=str(variables[command.split('=')[0]]['numerator'])+'/'+str(variables[command.split('=')[0]]['denominator'])
     else:
         evalInput = eval(command)
-             
+    
     if evalInput==None:
         return "Done."
     else:
         return evalInput
 
 def mode(var,fractionOrDecimal):
-    globals()[var]['mode']=fractionOrDecimal
+    variables[var]['mode']=fractionOrDecimal
 
 while True:
     print(commandLine(input()))
