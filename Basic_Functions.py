256b972b950cd50a4950ccac6c2baee59b62a5ae
grodinh <adamgrodin@A-Grodins-MacBook-Pro.local>
2014-04-06 09:37:58 -0400

    Added Support for Multiple Division Signs
    e.g. x=1/2/3

--- Basic_Functions.py
+++ Basic_Functions.py
@@ -24,7 +24,12 @@ def defineVariable(expression):
     if len(expression.split('/'))==1: #If variable value is a decimal
         variables[expression.split('=')[0]]={'mode':'decimal','decimal':float(fractions.Fraction(expression.split('=')[1])),'fraction':fractions.Fraction(expression.split('=')[1])}
     else: #If variable value is a fraction
-        variables[expression.split('=')[0]]={'mode':'fraction','decimal':float(fractions.Fraction(expression.split('=')[1])),'fraction':fractions.Fraction(expression.split('=')[1])}
+        last=fractions.Fraction((expression.split('=')[1]).split('/')[0])
+        for i in range(len((expression.split('=')[1]).split('/'))-1):
+            last=last/fractions.Fraction((expression.split('=')[1]).split('/')[i+1])
+        last=fractions.Fraction(last)
+        #variables[expression.split('=')[0]]={'mode':'fraction','decimal':float(fractions.Fraction(expression.split('=')[1])),'fraction':fractions.Fraction(expression.split('=')[1])}
+        variables[expression.split('=')[0]]={'mode':'fraction','decimal':float(last),'fraction':last}
     globals()[expression.split('=')[0]]=variables[expression.split('=')[0]][variables[expression.split('=')[0]]['mode']]
 
 def mode(var,fractionOrDecimal):
