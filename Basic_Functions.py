f746eded89a7b028065aaf05ac6fcb05cb764f5a
grodinh <adamgrodin@A-Grodins-MacBook-Pro.local>
2014-04-05 21:56:28 -0400

    Added Decimal-Fraction Integration
    Variables are now defined as a dictionary, with parts currently:
    
    'mode' for whether it is a fraction or decimal
    'decimal' for the decimal value of a number
    'numerator' for the numerator of a number
    'denominator' for the denominator of a number
    
    When a variable is recalled, the value of it is given based on what
    mode it is in.

--- Basic_Functions.py
+++ Basic_Functions.py
@@ -8,9 +8,20 @@ import fractions
 #Command Line Interface
 
 def commandLine(command):
-    if len(command.split("="))==2:
-        globals()[command.split("=")[0]]=int(command.split("=")[1])
+    if len(command.split('='))==2: #If defining a variable
+        if len(command.split('/'))==1: #If variable value is a decimal
+            globals()[command.split('=')[0]]={'mode':'decimal','decimal':float(command.split('=')[1]),'numerator':int(str(fractions.Fraction(str(command.split('=')[1]))).split('/')[0]),'denominator':int(str(fractions.Fraction(str(command.split('=')[1]))).split('/')[1])}
+        else: #If variable value is a fraction
+            globals()[command.split('=')[0]]={'mode':'fraction','decimal':float(eval(command.split('=')[1])),'numerator':int(str(fractions.Fraction(command.split('=')[1])).split('/')[0]),'denominator':int(str(fractions.Fraction(command.split('=')[1])).split('/')[1])}
+            print(globals()[command.split('=')[0]]['decimal'])
+            print(globals()[command.split('=')[0]]['numerator'])
+            print(globals()[command.split('=')[0]]['denominator'])
         evalInput="Done."
+    elif str(command) in globals():
+        if globals()[command]['mode']=='fraction':
+            evalInput=str(globals()[command]['numerator'])+"/"+str(globals()[command]['denominator'])
+        elif globals()[command]['mode']=='decimal':
+            evalInput=globals()[command]['decimal']
     else:
         evalInput = eval(command)
              
