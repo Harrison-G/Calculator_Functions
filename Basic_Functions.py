5cef97567a08d1fc59aecc47662cb14c9a4ec081
grodinh <adamgrodin@A-Grodins-MacBook-Pro.local>
2014-04-06 09:02:12 -0400

    Made Variables Use a fractions.Fraction()
    Variables now have a dictionary of:
    'mode' = whether the variable is a fraction or decimal
    'decimal' = the decimal representation of the variable
    'fraction' = the fractions.Fraction() of the variable
    
    The switch to a fractions.Fraction() rather than a numerator and
    denominator allows fractional variables to work in expressions.

--- Basic_Functions.py
+++ Basic_Functions.py
@@ -11,17 +11,11 @@ variables = {}
 def commandLine(command):
     if len(command.split('='))==2: #If defining a variable
         if len(command.split('/'))==1: #If variable value is a decimal
-            if float(command.split('=')[1])%1==0:
-                variables[command.split('=')[0]]={'mode':'decimal','decimal':float(command.split('=')[1]),'numerator':int(str(fractions.Fraction(str(command.split('=')[1]))).split('/')[0]),'denominator':1}
-            else:
-                variables[command.split('=')[0]]={'mode':'decimal','decimal':float(command.split('=')[1]),'numerator':int(str(fractions.Fraction(str(command.split('=')[1]))).split('/')[0]),'denominator':int(str(fractions.Fraction(str(command.split('=')[1]))).split('/')[1])}
-            globals()[command.split('=')[0]]=variables[command.split('=')[0]]['decimal']
+            variables[command.split('=')[0]]={'mode':'decimal','decimal':float(fractions.Fraction(command.split('=')[1])),'fraction':fractions.Fraction(command.split('=')[1])}
         else: #If variable value is a fraction
-            variables[command.split('=')[0]]={'mode':'fraction','decimal':float(eval(command.split('=')[1])),'numerator':int(str(fractions.Fraction(command.split('=')[1])).split('/')[0]),'denominator':int(str(fractions.Fraction(command.split('=')[1])).split('/')[1])}
-            globals()[command.split('=')[0]]=variables[command.split('=')[0]]['decimal'] #globals()[command.split('=')[0]]=str(variables[command.split('=')[0]]['numerator'])+'/'+str(variables[command.split('=')[0]]['denominator'])
+            variables[command.split('=')[0]]={'mode':'fraction','decimal':float(fractions.Fraction(command.split('=')[1])),'fraction':fractions.Fraction(command.split('=')[1])}
+        globals()[command.split('=')[0]]=variables[command.split('=')[0]][variables[command.split('=')[0]]['mode']]
         evalInput="Done."
-    elif str(command) in variables and variables[command]['mode']=='fraction':
-        evalInput=str(variables[command.split('=')[0]]['numerator'])+'/'+str(variables[command.split('=')[0]]['denominator'])
     else:
         evalInput = eval(command)
     
@@ -32,6 +26,8 @@ def commandLine(command):
 
 def mode(var,fractionOrDecimal):
     variables[var]['mode']=fractionOrDecimal
+    globals()[var]=variables[var][variables[var]['mode']]
+    return "Done."
 
 while True:
     print(commandLine(input()))
