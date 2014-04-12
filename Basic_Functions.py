01f439ee908a9abed6e0561d2c76a2bc90a61c45
grodinh <adamgrodin@A-Grodins-MacBook-Pro.local>
2014-04-11 22:30:41 -0400

    Major Overhaul on Variables
    Made fractions and decimals work MUCH better because of conversion with
    Fraction.limit_denominator() and Decimal().

--- Basic_Functions.py
+++ Basic_Functions.py
@@ -7,30 +7,10 @@ variables = {}
 #Command Line Interface
 
 def commandLine(command):
-    if len(command.split(' = '))==2: #If defining a variable
-        decimal=0
-        command_split=(command.split(' = ')[1]).split()
-        for i in range(len(command_split)):
-            print(command_split[i])
-            if command_split[i] in globals():
-                print(command_split[i])
-                if variables[command_split[i]]['mode']==decimal:
-                    decimal=1
-                command_split[i] = globals()[command_split[i]]
-                print(command_split[i])
-        print(command_split)
-        command_define=command.split(' = ')[0] + ' = '
-        for i in command_split:
-            command_define += str(i)
-        command = command_define
+    if len(command.split('='))==2: #If defining a variable
         defineVariable(command)
         evalInput="Done."
     else:
-        for i in command.split():
-            if i in globals():
-                print(i)
-                i = globals()[i]
-                print(i)
         evalInput = eval(command)
     
     if evalInput==None:
@@ -39,56 +19,11 @@ def commandLine(command):
         return evalInput
 
 def defineVariable(expression):
-    if len(expression.split('/'))==1: #If variable value is a decimal
-        myMode=''
-        if len(expression.split('.'))==1 and (expression.split(' = ')[0] in variables.keys()):
-            myMode='fraction'
-        else:
-            myMode='decimal'
-        variables[expression.split(' = ')[0]]={'mode':myMode,'decimal':float(fractions.Fraction(eval(expression.split(' = ')[1]))),'fraction':fractions.Fraction(eval(expression.split(' = ')[1]))}
-    else: #If variable value is a fraction
-        z = eval(expression.split(' = ')[1])
-        fraction = fractions.Fraction(z)#(expression.split(' = ')[1]))
-        numerator = fraction.numerator
-        denominator = fraction.denominator
-        numerator = fraction.numerator
-        denominator = fraction.denominator
-        non_repeating=''
-        repeating=''
-        ########
-        decimal_found = False
-        v = numerator // denominator
-        numerator = 10 * (numerator - v * denominator)
-        answer = str(v)
-        if numerator == 0:
-            return answer
-        answer += '.'
-        states = {}
-        while numerator > 0 :
-            prev_state = states.get(numerator, None)
-            if prev_state != None:
-                start_repeat_index = prev_state
-                non_repeating = answer[:start_repeat_index]
-                repeating = answer[start_repeat_index:]
-                #print(non_repeating + '[' + repeating + ']')
-                break
-            else:
-                non_repeating = (numerator/denominator)/10
-            states[numerator] = len(answer)
-            v = numerator // denominator
-            answer += str(v)
-            numerator -= v * denominator
-            numerator *= 10
-        ########
-        print(fraction)
-##        last=fractions.Fraction(eval((expression.split(' = ')[1]).split('/')[0]))
-##        for i in range(len((expression.split(' = ')[1]).split('/'))-1):
-##            last=last/fractions.Fraction((str(expression.split(' = ')[1])).split('/')[i])
-##        last=fractions.Fraction(last)
-        last = fraction
-        variables[expression.split(' = ')[0]]={'mode':'fraction','decimal':float(last),'fraction':last}
-    globals()[expression.split(' = ')[0]]=variables[expression.split(' = ')[0]][variables[expression.split(' = ')[0]]['mode']]
-    print(globals()[expression.split(' = ')[0]])
+    variables[expression.split('=')[0]]={'mode':'decimal','decimal':float(eval(expression.split('=')[1])),'fraction':fractions.Fraction(eval(expression.split('=')[1])).limit_denominator(10**8)}
+    if '/' in expression.split('=')[1]:
+        variables[expression.split('=')[0]]['mode']='fraction'
+    globals()[expression.split('=')[0]]=variables[expression.split('=')[0]][variables[expression.split('=')[0]]['mode']]
+    print(globals()[expression.split('=')[0]])
 
 def mode(var,fractionOrDecimal):
     variables[var]['mode']=fractionOrDecimal
